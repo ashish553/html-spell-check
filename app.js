ce2482b71a8909719e096b36cf5f127807d00d61
Ashish_Sharma <sashish359@gmail.com>
2022-03-06 14:45:56 +0530

    line numbr added and module created

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const { convert } = require('html-to-text');
-const spell = require('spell-checker-js')
+const spell = require('./spell-check/spell-check')
 spell.load('en')
 
+// get filtered html data without newlines nd return char.
 function getArrayOfDataInFile(filename){
     let allLinesArr = []
     let data = fs.readFileSync(filename, 'utf-8')
@@ -15,6 +16,14 @@ function getArrayOfDataInFile(filename){
     return allLinesArr
 }
 
+// delete the file that has been created by fs module for filtering the html data.
+function delFile(filename){
+    fs.unlink(filename, (err) => {
+            if (err) throw err;
+            // console.log(`${filename} was deleted`);
+        });
+}
+
 let a
 try{
     let allLines = getArrayOfDataInFile('./testFile.html')
@@ -22,17 +31,25 @@ try{
         fs.writeFileSync('./writenByApp.txt',eachOne+'\r\n',{ flag: 'a+' })
     });
     let newData = getArrayOfDataInFile('./writenByApp.txt')
+    let lineNumber = 0, incorrectLineNumberArray = []
     newData.forEach(eachLine => {
+        lineNumber += 1
         if(convert(eachLine)!==''){
             fs.writeFileSync('./textc.txt',convert(eachLine).replaceAll('\n',' ')+'\r\n',{flag: 'a+'})
+            incorrectLineNumberArray.push(lineNumber)
         }
     });
     let htmlTagTextArray = getArrayOfDataInFile('./textc.txt')
+    // console.log(htmlTagTextArray);
     let incorrectWordist
-    htmlTagTextArray.forEach(eachTagText => {
-        incorrectWordist = spell.check(eachTagText)
-        console.log(incorrectWordist)
-    });
+    for(let i=0;i<htmlTagTextArray.length;++i){
+        incorrectWordist = spell.check(htmlTagTextArray[i])
+        // if(spell.check(htmlTagTextArray[i])!==[]){
+            console.log(incorrectWordist+" : "+incorrectLineNumberArray[i])
+        // }
+    }
+    delFile('./textc.txt')
+    delFile('./writenByApp.txt')
 }catch(err){
     console.log(err);
 }
