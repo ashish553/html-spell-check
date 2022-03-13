aa575de2a8bcc7e9a9323db275dba0e5e9ba2252
Ashish_Sharma <sashish359@gmail.com>
2022-03-13 22:04:52 +0530

    gamma for grammer chk implemented(ip)

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 var XRegExp = require("xregexp");
 const spell = require('./spell-check/spell-check')
+var writeGood = require('write-good');
+const gramma = require('gramma');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+
 spell.load('en')
 
-// get filtered html data without newlines nd return char.
+// get filtered html data without newlines and return char.
 function getArrayOfDataInFile(filename){
     let allLinesArr = []
     let data = fs.readFileSync(filename, 'utf-8')
@@ -20,7 +25,6 @@ function getArrayOfDataInFile(filename){
 function delFile(filename){
     fs.unlink(filename, (err) => {
             if (err) throw err;
-            // console.log(`${filename} was deleted`);
         });
 }
 
@@ -32,7 +36,13 @@ function html2text(htmlString){
     return htmlString.replace(removeTags,' ').replace(removeSpecialChar,' ').replace(removeWhitespaces,' ')
 }
 
-let a
+
+function html2textWithPunctuation(htmlString){
+    let removeTags = XRegExp('(<([^>]+)>)','g') //remove all tags with/without attributes. Ex - <html></html>, <div></div>, <a href="www">,
+    let removeWhitespaces = XRegExp(' {2,}','g') //select extra whitespaces
+    return htmlString.replace(removeTags,' ').replace(removeWhitespaces,' ')
+}
+
 try{
     let allLines = getArrayOfDataInFile('./testFile.html')
     allLines.forEach(eachOne => {
@@ -55,8 +65,59 @@ try{
             console.log(incorrectWordist+" : "+incorrectLineNumberArray[i])
         }
     }
+    let readForGrammar = getArrayOfDataInFile('./writenByApp.txt')
+    // let lineNumberForP = 0
+    incorrectLineNumberArray = []
+    readForGrammar.forEach(eachLine => {
+        // lineNumberForP += 1
+        // if(html2text(eachLine)!==''){
+            fs.writeFileSync('./textcForP.txt',html2textWithPunctuation(eachLine)+'\r\n',{flag: 'a+'})
+            incorrectLineNumberArray.push(lineNumber)
+        // }
+    });
+    let dataForP = getArrayOfDataInFile('./textcForP.txt');
+    let lineNumberForP = 0
+    let lineNumberForPArr = []
+    const matchesArr= []
+    for(let i=0;i<dataForP.length;i++){
+        if(dataForP[i]!==''){
+            allErrors = gramma.check(dataForP[i]).then((result) => {
+                lineNumberForP += 1
+                matchesArr.push(result.matches)
+                lineNumberForPArr.push(i+1)
+                return matchesArr
+            })
+
+        }
+    }
+    const printAllErrors = ()=>{
+        allErrors.then((errorsWeGot)=>{
+            console.log(errorsWeGot);
+            console.log(lineNumberForPArr);
+        })
+    }
+    printAllErrors()
+    console.log(lineNumberForPArr)
+    function getGrammaErrors(dataForP,i,lineNumberForP){
+        
+        return new Promise((resolve, rejects)=>{
+            gramma.check(dataForP[i]).then((result)=>{
+                hehe[lineNumberForP] = result
+                lineNumberForPArr.push(lineNumberForP)
+            })
+            const error = false
+            if(!error){
+                resolve()
+            }else{
+                rejects()
+            }
+        })
+    }
+    
+    delFile('./textcForP.txt')
     delFile('./textc.txt')
     delFile('./writenByApp.txt')
 }catch(err){
     console.log(err);
 }
+
